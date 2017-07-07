32f8e9388d90cd7dd0a640a626b0499846ce922a
HuangYanLi <339600718@qq.com>
2017-07-07 17:38:44 +0800

    lily
    test commit.py

---
+++ lilytestssssss/commit.py
@@ -0,0 +1,20 @@
+
+
+print(333)
+print(333)
+print(333)
+print(333)
+print(333)
+print(333)
+print(333)
+print(333)
+print(333)
+print(333)
+#在这创建了文件 写入了内容  现在要去github 桌面软件进行文件提交
+
+# 打开github桌面版  点击changes
+# 吧当前新加入的文件在下方
+# Summary  --用户
+# Description --提交内容 /
+# 注意 .idea开头的 ，不用提交，不勾选
+# 只提交你新增加或者修改的文件，其他不其他叫
