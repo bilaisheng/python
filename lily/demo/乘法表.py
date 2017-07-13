72a3df85d5f40d53aacba33d44e097d1f3546f60
HuangYanLi <339600718@qq.com>
2017-07-13 20:22:51 +0800

    乘法表
    乘法表

--- lily/demo/乘法表.py
+++ lily/demo/乘法表.py
@@ -1,8 +1,8 @@
-#！/usr/bin/python3
+# ！/usr/bin/python3
 # Filename : chengfabiao.py
 # Author by : Lily
 
-#实现乘法表
+# 实现乘法表
 for i in range(1,10):
     for j in range(1,i+1):
         print("%d * %d = %d" %(j,i,i*j),end="  ")
