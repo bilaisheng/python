2a79b09e312d723f57ea8c365af8929796e6f388
HuangYanLi <339600718@qq.com>
2017-07-26 21:04:30 +0800

    downlaod页面
    downlaod页面完结

--- lily/demo/download.py
+++ lily/demo/download.py
@@ -82,18 +82,18 @@ def download4(url, user_agent='test', num_retries=2):
 # 设置用户代理IP,proxy,代理IP;data是用户可输入的参数
 
 
-def download5(url, proxy=None, user_agent='test', num_retries=2, data=None):
+def download5(url, free_proxy=None, user_agent='test', num_retries=2, data=None):
     print("download5...", url)
     # 设置headers 中的用户代理，默认值是test
-    headers = {"User_agent":user_agent}
+    headers = {"User_agent": user_agent}
     # 将用户代理添加到请求中
     request = urllib.request.Request(url, data, headers=headers)
     # 创建句柄
     opener = urllib.request.build_opener()
     # 判断如果proxy是否有值
-    if proxy:
+    if free_proxy:
         # 获取ip代理协议和IP代理
-        proxy_params = {urllib.request.urlparse(url).scheme: proxy}
+        proxy_params = {urllib.request.urlparse(url).scheme: free_proxy}
         # 将IP代理设置添加到句柄中
         opener.add_handler(urllib.request.ProxyHandler(proxy_params))
     try:
@@ -102,14 +102,14 @@ def download5(url, proxy=None, user_agent='test', num_retries=2, data=None):
     # 异常处理，捕获异常
     except urllib.request.URLError as e:
         # 打印异常原因
-        print("download error",e.reason)
+        print("download error", e.reason)
         html5 = None
         # 判断重新加载次数是否大于0
         if num_retries > 0:
             # 判断 页面code是否在500和600之间
             if hasattr(e, 'code') and 500 <= e.code < 600:
                 # 调用自身
-                html5 = download5(url, proxy, user_agent, num_retries-1)
+                html5 = download5(url, free_proxy, user_agent, num_retries - 1)
     return html5
 
 # 百度首页
@@ -119,22 +119,22 @@ url2 = 'http://www.baidu.com'
 url1 = 'http://httpstat.us/500'
 
 # 下载百度首页，设置用户代理为360浏览器
-html = download4(url2, user_agent='"360":"User-Agent: Mozilla/4.0 (compatible; MSIE 7.0;Window NT 5.1;360SE)"')
+html = download4(url2, user_agent='"360":"User-Agent: Mozilla/4.0 (compatible; MS IE 7.0;Window NT 5.1;360SE)"')
 
 # 下载百度首页，设置用户代理为UC浏览器
-html1 = download4(url2, user_agent='"UC":"User-Agent:UCWEB7.0.2.37/28/999"')
+html1 = download4(url2, user_agent='"UC":"User-Agent:UC WEB7.0.2.37/28/999"')
 
 # 打开pc.html,如路径中无此文件会自动创建
-myfile = open('pc.html', "w+", encoding="utf-8")
+first_file = open('pc.html', "w+", encoding="utf-8")
 
 # 打开mb.html,如果路径中无此文件会自动创建
-myfile1 = open("mb.html", "w+", encoding="utf-8")
+your_file = open("mb.html", "w+", encoding="utf-8")
 
 # 将下载到的内容写入pc.html(使用360代理)
-myfile.write(html.decode("utf-8"))
+first_file.write(html.decode("utf-8"))
 
 # 将下载到的内容写入mb.html（使用UC代理）
-myfile1.write(html1.decode("utf-8"))
+your_file.write(html1.decode("utf-8"))
 
 # 设置 IP代理
 proxy = '117.90.5.133:9000'
