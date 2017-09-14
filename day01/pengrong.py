66974835806d5c067b22a565329c26b15f1b393e
JKAK47 <JKAK47@users.noreply.github.com>
2017-09-14 20:37:20 +0800

    day01
    pengrong

---
+++ day01/pengrong.py
@@ -0,0 +1,26 @@
+'''
+
+1 数据类型 (类型转换 .每个类型常用方法, )
+2 字符操作(截取 拼接,替换 等)
+3 循环(for ,  for else , while ,while else 常用循环判断)
+'''
+
+# 基本数据类型 整数， 浮点型， 字符串
+ # 整数
+inta=10
+intb=21
+print (inta+intb) # 30
+print (intb/inta) # 2.1
+print (inta/intb) #0.47619047619047616
+print (intb*inta) # 210
+# 浮点型，科学计数法;非精确
+ef=2.1*10e10
+print (ef)
+ef=ef*2.5654
+print (ef)
+# 字符串
+
+# 布尔类型
+
+
+# 集合类数据类型
