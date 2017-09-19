8a276bd7031ec8f9f024a7c9f46235199dae4c67
878799579 <878799579@qq.com>
2017-09-19 09:31:41 +0800

    commit

--- godliness/czyDemo/3.py
+++
@@ -1,9 +0,0 @@
-#取却对值
-def my_abs(x):
-    if x>=0:
-        return x
-    else:
-        return -x
-
-print(my_abs(-20))
-
--- godliness/czyDemo/使用map.py
+++
@@ -1,8 +0,0 @@
-#将一个函数作用在一个list上面
-def f(x):
-    return x * x
-listA = range(10)
-a = map(f,listA)
-
-print(a)
-
