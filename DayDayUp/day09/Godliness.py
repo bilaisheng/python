a6600dff45dd4c6f1a8f633fe296eb936fe33766
bilaisheng <878799579@qq.com>
2017-09-29 10:32:56 +0800

    commit

---
+++ DayDayUp/day09/Godliness.py
@@ -0,0 +1,169 @@
+# 错误、调试和测试：错误处理、调试、单元测试和文档测试
+
+# try:
+#     print('try...')
+#     r = 10 / 0  # 除数不能为0
+#     print('result:', r)
+# except ZeroDivisionError as e:
+#     print('except:', e)
+# finally:
+#     print('finally...')
+# print('END')
+#
+#
+# try:
+#     print('try...')
+#     r = 10 / int('a')
+#     print('result:', r)
+# except ValueError as e:
+#     print('ValueError:', e)
+# except ZeroDivisionError as e:
+#     print('ZeroDivisionError:', e)
+# finally:
+#     print('finally...')
+# print('END')
+#
+#
+# try:
+#     print('try...')
+#     r = 10 / int('2')
+#     print('result:', r)
+# except ValueError as e:
+#     print('ValueError:', e)
+# except ZeroDivisionError as e:
+#     print('ZeroDivisionError:', e)
+# else:
+#     print('no error')
+# finally:
+#     print('finally...')
+# print('END')
+
+'''
+使用try...except捕获错误还有一个巨大的好处，就是可以跨越多层调用，
+比如函数main()调用foo()，foo()调用bar()，结果bar()出错了，这时，
+只要main()捕获到了，就可以处理
+'''
+
+
+# def foo(s):
+#     return 10 / int(s)
+#
+#
+# def bar(s):
+#     return foo(s) * 2
+#
+#
+# def main():
+#     try:
+#         bar('0')
+#     except Exception as e:
+#         print('Error:', e)
+#     finally:
+#         print('finally...')
+#
+# main()
+
+# import logging
+#
+#
+# def foo(s):
+#     return 10 / int(s)
+#
+#
+# def bar(s):
+#     return foo(s) * 2
+#
+#
+# def main():
+#     try:
+#         bar('0')
+#     except Exception as e:
+#         logging.exception(e)
+#
+# main()
+# print('END')
+
+# 断言
+# def foo(s):
+#     n = int(s)
+#     assert n != 0, 'n is zero!'
+#     return 10 / n
+#
+#
+# def main():
+#     foo('0')
+#
+# main()
+
+# import logging
+#
+# logging.basicConfig(level=logging.INFO)
+#
+# s = '0'
+# n = int(s)
+# logging.info('n = %d' % n)
+# print(10 / n)
+
+
+# import pdb
+#
+# s = '0'
+# n = int(s)
+# pdb.set_trace() # 运行到这里会自动暂停
+# print(10 / n)
+
+class Dict(dict):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError(r'Dict object has no attribute %s' % item)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+import unittest
+
+class TestDict(unittest.TestCase):
+
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+class TestDict(unittest.TestCase):
+
+    def setUp(self):
+        print('setUp...')
+
+    def tearDown(self):
+        print('tearDown...')
