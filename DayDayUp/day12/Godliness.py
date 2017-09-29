6b875d1d00f698440ed05547b3be09c6ef6e586a
bilaisheng <878799579@qq.com>
2017-09-29 21:49:06 +0800

    commmit

--- DayDayUp/day12/Godliness.py
+++ DayDayUp/day12/Godliness.py
@@ -366,7 +366,6 @@ print(parser.Parse(xml))
 #     print('Data:', f.read().decode('utf-8'))
 
 
-
 from urllib import request, parse
 
 print('Login to weibo.cn...')
@@ -414,8 +413,6 @@ with request.urlopen(req, data=login_data.encode('utf-8')) as f:
 # im2 = im.filter(ImageFilter.BLUR)
 # im2.save('blur.jpg', 'jpeg')
 
-
-
 import random
 
 def generate_verification_code_two():
@@ -451,11 +448,11 @@ def generate_verification_code(len=6):
     # 注意： 这里我们生成的是0-9A-Za-z的列表，当然你也可以指定这个list，这里很灵活
     # 比如： code_list = ['P','y','t','h','o','n','T','a','b'] # PythonTab的字母
     code_list = []
-    for i in range(10): # 0-9数字
+    for i in range(10):  # 0-9数字
         code_list.append(str(i))
-    for i in range(65, 91): # 对应从“A”到“Z”的ASCII码
+    for i in range(65, 91):   # 对应从“A”到“Z”的ASCII码
         code_list.append(chr(i))
-    for i in range(97, 123): #对应从“a”到“z”的ASCII码
+    for i in range(97, 123):  # 对应从“a”到“z”的ASCII码
         code_list.append(chr(i))
     myslice = random.sample(code_list, len)  # 从list中随机获取6个元素，作为一个片断返回
     verification_code = ''.join(myslice) # list to string
